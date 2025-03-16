ba718bef2c5e6f676860b63b06bced7069543fcc
Pavan kadavath <144763372+pavankadavath@users.noreply.github.com>
2025-03-17 01:23:33 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import './Header.css'; 
+import React, { useState } from 'react';
+import './Header.css';
 import logo from '../Assets/logo.png';
 import { Link, useNavigate } from 'react-router-dom';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
+
   return (
     <header className="header">
       <div className="container">
         <div className="logo">
           <img src={logo} alt="Communion App Logo" onClick={() => navigate('/')} />
         </div>
-        <nav className="navbar">
+
+        {/* Hamburger Menu Button */}
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+
+        {/* Navbar Links */}
+        <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
           <ul className="nav-links">
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
             </li>
             <li className="dropdown">
-              <Link to="/events">Events</Link>
+              <Link to="/events" onClick={() => setMenuOpen(false)}>Events</Link>
               <ul className="dropdown-menu">
-                <li>
-                  <Link to="/events">Festivals</Link>
-                </li>
-                <li>
-                  <Link to="/events">Social</Link>
-                </li>
-                {/* <li>
-                  <Link to="/events/charity">Charity</Link>
-                </li> */}
-                <li>
-                  <Link to="/events">Contests</Link>
-                </li>
-                <li>
-                  <Link to="/register">Add Event</Link>
-                </li>
+                <li><Link to="/events" onClick={() => setMenuOpen(false)}>Festivals</Link></li>
+                <li><Link to="/events" onClick={() => setMenuOpen(false)}>Social</Link></li>
+                <li><Link to="/events" onClick={() => setMenuOpen(false)}>Contests</Link></li>
+                <li><Link to="/register" onClick={() => setMenuOpen(false)}>Add Event</Link></li>
               </ul>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
             </li>
           </ul>
         </nav>
